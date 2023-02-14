d6a6469c0179e536173a21b9f690b410184c6600
dylanwares <c1012044@my.shu.ac.uk>
2023-02-14 15:27:10 +0000

    routing train from api

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,39 @@
-function expandMenu(){
+function expandMenu(e){
     var secondMenu = document.getElementById('secondMenu');
     secondMenu.style.display = 'inline';
+    const headers = new Headers();
+    headers.append('X-ApiKey', 'AA26F453-D34D-4EFC-9DC8-F63625B67F4A');
+    headers.append('X-ApiVersion', '1');
+    // var tiploc = e.currentTarget.id;
+    var tiploc = 'THMSLGB';
+    fetch('https://traindata-stag-api.railsmart.io/api/trains/tiploc/' + tiploc + '/2023-02-13 00:00:00/2023-02-13 23:59:59', {headers: headers})
+        .then (res => res.json())
+        .then (data => {
+            var container = document.getElementById('secondMenuItems');
+            for (const item of data) {
+                var p = document.createElement('p');
+                p.innerHTML = item.originLocation + ' - ' + item.destinationLocation;
+                p.activationId = item.activationId;
+                p.scheduleId = item.scheduleId;
+                p.style.fontSize = '14px';
+                p.addEventListener("click", journeyClicked);
+                p.classList.add('menuOptions');
+                container.append(p);
+            }
+        })
 }
 
 function closeSecondMenu(){
     secondMenu.style.display = 'none';
+}
+
+function journeyClicked(e){
+    closeSecondMenu();
+    map.eachLayer(function(layer){
+        if (layer != Location.tileLayer){
+        map.removeLayer(layer);
+        }
+    })
+    addTileLayer();
+    route(e);
 }
--- js/map.js
+++ js/map.js
@@ -3,7 +3,11 @@
 var map = L.map('map').setView([52.4710531, -2.4481026], 6.69);
 
 // Style URL format in XYZ PNG format; see our documentation for more options
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
-    maxZoom: 20,
-    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
-}).addTo(map);
+function addTileLayer(){
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+        maxZoom: 20,
+        attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+    }).addTo(map);
+}
+
+addTileLayer();
