484cb7aff0d8ec965357aac5826667f22e9777ff
vMiksta <miksta100@live.com.au>
2023-02-15 18:55:04 +1100

    Edits
    Signed-off-by: vMiksta <miksta100@live.com.au>

---
+++ v1/fetchLocalApi.js
@@ -0,0 +1,13 @@
+//Fetch from local host API
+
+import fs from 'fs';
+
+fetch(`http://localhost:3000/api`)
+  .then(res => res.json())
+  .then(data => {
+    fs.writeFileSync(`localData.json`, JSON.stringify(data, null, 2), 'utf-8');
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
--- v1/index.js
+++ v1/index.js
@@ -3,12 +3,19 @@ import data from './locationID.json' assert { type: "json" };;
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send("Trains");
+});
+
+//Origin, Destination, sID, aID to server/data
+
+app.get('/api', (req, res) => {
+  res.send(data);
+});
+
 app.listen(3000, () => {
     console.log('On');
   });
 
-  app.get('/', (req, res) => {
-  //Origin, Destination, sID, aID to server
-  res.send(data);
-});
+
 
