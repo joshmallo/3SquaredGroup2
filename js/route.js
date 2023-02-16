3be246d7a6a738eabbd2cb0851c1b0fa82cb8961
Casey Cheung <62992468+casey-tk-cheung@users.noreply.github.com>
2023-02-16 16:06:58 +0000

    updated route file

--- js/route.js
+++ js/route.js
@@ -1,29 +1,42 @@
-function route(e) {
+async function route(e) {
+    var activationId = e.currentTarget.activationId;
+    var scheduleId = e.currentTarget.scheduleId;
     var route = [];
     var left = [];
     const headers = new Headers();
     headers.append('X-ApiKey', 'AA26F453-D34D-4EFC-9DC8-F63625B67F4A');
     headers.append('X-ApiVersion', '1');
 
-    // console.log('https://traindata-stag-api.railsmart.io/api/ifmtrains/movement/' + e.currentTarget.activationId + '/' + e.currentTarget.scheduleId);
+    console.log('https://traindata-stag-api.railsmart.io/api/ifmtrains/movement/' + e.currentTarget.activationId + '/' + e.currentTarget.scheduleId);
     var lastVisitedTiploc;
-    var response = fetch('https://traindata-stag-api.railsmart.io/api/ifmtrains/schedule/' + e.currentTarget.activationId + '/' + e.currentTarget.scheduleId, {headers: headers})
+    await fetch('https://traindata-stag-api.railsmart.io/api/ifmtrains/movement/' + e.currentTarget.activationId + '/' + e.currentTarget.scheduleId, { headers: headers })
         .then(response => response.json())
         .then(data => {
-            var i = 0;
+            if (data.length != 0)
+            lastVisitedTiploc = data[data.length - 1].tiploc;
+            else lastVisitedTiploc = 0;
+        })
+    fetch('https://traindata-stag-api.railsmart.io/api/ifmtrains/schedule/' + activationId + '/' + scheduleId, {headers: headers})
+        .then(response => response.json())
+        .then(data => {
+            while (!data[data.length - 1].hasOwnProperty('latLong')) data.pop()
+            var completedJourney = true;
             for (const item of data) {
-                i++;
                 if (item.hasOwnProperty('latLong')) {
-                    var latlng = [item.latLong.latitude, item.latLong.longitude];
-                    if (i < 22) {
-                        route.push(latlng);
+                    if (lastVisitedTiploc == 0) {
+                        lastVisitedTiploc = data[data.indexOf(item)].tiploc;
                     }
-                    else if (i == 22) {
+                    var latlng = [item.latLong.latitude, item.latLong.longitude];
+                    if (completedJourney) { 
                         route.push(latlng);
-                        left.push(latlng);
                     }
                     else {
                         left.push(latlng)
+                    }
+                    if (item.tiploc == lastVisitedTiploc) // finds last visited tiploc
+                    {
+                        completedJourney = false;
+                        left.push(latlng);
                     }
                 }
                 if (item.hasOwnProperty('latLong') && item.hasOwnProperty('departure') && (item != data[0] && item != data[data.length[-1]])) {
@@ -31,42 +44,48 @@ function route(e) {
                         .addTo(map)
                         .bindPopup(item.location);
                 }
+
             }
-            var fullRoute = route.concat(left);
+            // var fullRoute = route.concat(left);
             // var movingMarker = L.Marker.movingMarker([route[0], left[left.length - 1]],
             //     [5000]).addTo(map);
             // movingMarker.start();
             // console.log(fullRoute);
             new L.marker(route[0]).bindPopup(data[0].location).addTo(map);
+            new L.marker(left[0], {icon: train}).bindPopup('Last updated location').addTo(map);
             new L.marker(left[left.length - 1]).bindPopup(data[data.length - 1].location).addTo(map);
-            const path = L.polyline.antPath(route, {
-                "delay": 800,
-                "dashArray": [
-                    10,
-                    20
-                ],
-                "weight": 5,
-                "color": "#00FF00",
-                "pulseColor": "#FFFFFF",
-                "paused": false,
-                "reverse": false,
-                "hardwareAccelerated": true
-            });
-            map.addLayer(path);
-            const path2 = L.polyline.antPath(left, {
-                "delay": 800,
-                "dashArray": [
-                    10,
-                    20
-                ],
-                "weight": 5,
-                "color": "#0000FF",
-                "pulseColor": "#FFFFFF",
-                "paused": false,
-                "reverse": false,
-                "hardwareAccelerated": true
-            });
-            map.addLayer(path2);
+            if (route.length != 0){
+                const path = L.polyline.antPath(route, { // completed journey
+                    "delay": 800,
+                    "dashArray": [
+                        10,
+                        20
+                    ],
+                    "weight": 5,
+                    "color": "#00FF00",
+                    "pulseColor": "#FFFFFF",
+                    "paused": false,
+                    "reverse": false,
+                    "hardwareAccelerated": true
+                });
+                map.addLayer(path);
+            }
+            if (left.length != 0){
+                const path2 = L.polyline.antPath(left, { // journey left to complete
+                    "delay": 800,
+                    "dashArray": [
+                        10,
+                        20
+                    ],
+                    "weight": 5,
+                    "color": "#0000FF",
+                    "pulseColor": "#FFFFFF",
+                    "paused": false,
+                    "reverse": false,
+                    "hardwareAccelerated": true
+                });
+                map.addLayer(path2);
+            }
         })
 
     var stationIcon = L.icon({
@@ -80,15 +99,15 @@ function route(e) {
         popupAnchor: [10, -15] // point from which the popup should open relative to the iconAnchor
     });
 
-    var trainIcon = L.icon({
-        iconUrl: 'assets/train.png',
-        shadowUrl: 'assets/train.png',
+    var train = L.icon({
+        iconUrl: 'assets\\train.png',
+        shadowUrl: 'assets\\train.png',
 
         iconSize: [24, 24], // size of the icon
         shadowSize: [0, 0], // size of the shadow
-        iconAnchor: [0, 10], // point of the icon which will correspond to marker's location
+        iconAnchor: [10, 25], // point of the icon which will correspond to marker's location
         shadowAnchor: [0, 0],  // the same for the shadow
-        popupAnchor: [10, -15] // point from which the popup should open relative to the iconAnchor
-    });
+        popupAnchor: [10, -15]
+    })
     // map.fitBounds(path);
 }
